098825a3bafd708e26c60a8713914c243a049809
Lean Valcke <leanvalcke@hotmail.com>
2020-04-15 00:03:43 +0200

    Nieuwe query overzicht bestllingen van ingelogde klant

--- inlog/bestelling.php
+++ inlog/bestelling.php
@@ -1,6 +1,12 @@
 <?php require('../config.php');
-$sql = "SELECT BonID, KlantID, Datum FROM tblbestelling";	
-$dbh->query($sql);?>
+//$sql = "SELECT BonID, KlantID, Datum FROM tblbestelling";	
+$sql = "SELECT tblbestelbons.id, tblbestelbons.tblklant_KlantID, tblbestelbons.besteldatum, tblbestelstatus.status FROM tblbestelbons
+INNER JOIN tblbestelstatus ON tblbestelstatus.id = tblbestelbons.status";	
+$result = $dbh->query($sql);
+
+var_dump($result->fetchAll());
+die();
+?>
 
 <html>
     <head>
